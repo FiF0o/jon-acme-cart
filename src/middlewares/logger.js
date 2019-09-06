dce33ccd3216d82c377bc36b128ad3f3f1c2d6a9
FiF0o <jonathan.lazarini@wipro.com>
2019-09-06 15:17:54 +0100

    add check on async action in logger mw

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,8 +1,12 @@
 export const logger = store => next => action => {
-  console.group(action.type)
+  
+  const actionType = typeof action === "function" ? `async action ${Date.now()}` : action.type
+
+  console.group(actionType)
   console.info('dispatching', action)
   let result = next(action)
   console.log('next state', store.getState())
-  console.groupEnd()
+  console.log('result', result)
+  console.groupEnd(actionType)
   return result 
 }
