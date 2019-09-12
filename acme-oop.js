ced3184ac1c5b0c99ce0c50618a3e60d86b2c7a5
FiF0o <jonathan.lazarini@wipro.com>
2019-09-12 15:45:24 +0100

    add buy1get1free rule

--- acme-oop.js
+++ acme-oop.js
@@ -1,5 +1,5 @@
+const buy1Get1Free = item => items => Math.floor(items.filter(i => i.code === item.code).length / 2) * item.price
 const buy3DropPrice = (item, price) => items => items.filter(i => i.code === item.code).length < 3 ? 0 : items.filter(i => i.code === item.code).length * (item.price - price)
-
 class Basket {
     constructor(rules) {
         this.rules = rules
@@ -10,13 +10,6 @@ class Basket {
 }
 
 const [fr1, sr1, cf1] = [{'code': 'FR1', 'name': 'Fruit tea', 'price': 3.11}, {'code': 'SR1', 'name': 'Strawberries', 'price': 5.00}, {'code': 'CF1', 'name': 'Coffee', 'price': 11.23}]
-
-console.log('case 1:')
-console.log(new Basket([buy3DropPrice(sr1, 4.50)]).add(fr1).add(sr1).add(fr1).add(cf1).total())
-console.log('\n')
-console.log('case 2:')
-console.log(new Basket([buy3DropPrice(sr1, 4.50)]).add(fr1).add(fr1).total())
-console.log('\n')
-console.log('case 3:')
-console.log(new Basket([buy3DropPrice(sr1, 4.50)]).add(sr1).add(sr1).add(fr1).add(sr1).total())
-console.log('\n')
+console.log(new Basket([buy1Get1Free(fr1), buy3DropPrice(sr1, 4.50)]).add(fr1).add(sr1).add(fr1).add(cf1).total())
+console.log(new Basket([buy1Get1Free(fr1), buy3DropPrice(sr1, 4.50)]).add(fr1).add(fr1).total())
+console.log(new Basket([buy1Get1Free(fr1), buy3DropPrice(sr1, 4.50)]).add(sr1).add(sr1).add(fr1).add(sr1).total())
