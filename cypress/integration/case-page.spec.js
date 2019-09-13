d64f27552825471106cd775eeb7b9f2d7e76e235
FiF0o <jonathan.lazarini@wipro.com>
2019-09-13 10:32:06 +0100

    add cypress deps, basic test and scafold

--- cypress/integration/case-page.spec.js
+++ cypress/integration/case-page.spec.js
@@ -1,5 +1,25 @@
+const mockData = require('../../server/api/db.json');
+
 describe('Case page', () => {
-  it('should visit the page', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
+  })
+
+  it('should visit the page', () => {
+    cy.server()
+    cy.route('GET', '/api/basketnormal',
+      [...mockData.basketNormal]
+    )
+
+    cy.get('[dataattribute="normal"]')
+      .focus()
+      .click()
+    // Test API call
+
+    // Test XHR response
+
+    // Test reducer has received the data
+
+    // Test that the UI is rendered
   })
 })
